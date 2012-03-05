afce6a1981c3794dbd7cab93cddd31dc1e191ffc
Jakob Pless <pless84@gmail.com>
2012-03-05 13:23:48 +0100

    New Tab Layout

---
+++ PhysicsApp/src/dk/pless84/physics/LogActivity.java
@@ -0,0 +1,11 @@
+package dk.pless84.physics;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LogActivity extends Activity {
+	 public void onCreate(Bundle savedInstanceState) {
+	        super.onCreate(savedInstanceState);
+	        setContentView(R.layout.log_layout);
+	    }
+}
---
+++ PhysicsApp/src/dk/pless84/physics/MeasureActivity.java
@@ -0,0 +1,11 @@
+package dk.pless84.physics;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MeasureActivity extends Activity {
+	 public void onCreate(Bundle savedInstanceState) {
+	        super.onCreate(savedInstanceState);
+	        setContentView(R.layout.measure_layout);
+	    }
+}
