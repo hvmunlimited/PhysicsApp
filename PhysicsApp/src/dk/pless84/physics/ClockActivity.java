871145ca7b6fc8de53ed4480d7a21e8a89c705cb
Jakob Pless <pless84@gmail.com>
2012-03-19 12:54:35 +0100

    New

---
+++ PhysicsApp/src/dk/pless84/physics/ClockActivity.java
@@ -0,0 +1,14 @@
+package dk.pless84.physics;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ClockActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.clock);
+	}
+
+}
