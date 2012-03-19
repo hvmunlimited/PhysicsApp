268907bcc377b6c2c687080796ba3333a1c21199
Jakob Pless <pless84@gmail.com>
2012-03-19 10:04:38 +0100

    Major edit

--- PhysicsApp/src/dk/pless84/physics/Main.java
+++ PhysicsApp/src/dk/pless84/physics/Main.java
@@ -1,10 +1,7 @@
 package dk.pless84.physics;
 
 import android.app.TabActivity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TabHost;
-import android.widget.TabHost.TabSpec;
 
 public class Main extends TabActivity {
     /** Called when the activity is first created. */
@@ -12,33 +9,5 @@ public class Main extends TabActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        
-        TabHost tabHost = getTabHost();
-        
-        // Tab for Measuring
-        String tmp = getResources().getString(R.string.measurements);
-        TabSpec measureSpec = tabHost.newTabSpec(tmp);
-        measureSpec.setIndicator(tmp, getResources().getDrawable(R.drawable.icon_measure_tab));
-        Intent measureIntent = new Intent(this, MeasureActivity.class);
-        measureSpec.setContent(measureIntent);
- 
-        // Tab for Tools
-        tmp = getResources().getString(R.string.tools);
-        TabSpec toolsSpec = tabHost.newTabSpec(tmp);
-        toolsSpec.setIndicator(tmp, getResources().getDrawable(R.drawable.icon_tools_tab));
-        Intent toolsIntent = new Intent(this, ToolsActivity.class);
-        toolsSpec.setContent(toolsIntent);
-        
-        // Tab for Log
-        tmp = getResources().getString(R.string.log);
-        TabSpec logSpec = tabHost.newTabSpec(tmp);
-        logSpec.setIndicator(tmp, getResources().getDrawable(R.drawable.icon_archive_tab));
-        Intent logIntent = new Intent(this, LogActivity.class);
-        logSpec.setContent(logIntent);
-
-        // Adding all TabSpec to TabHost
-        tabHost.addTab(measureSpec);
-        tabHost.addTab(toolsSpec);
-        tabHost.addTab(logSpec);
     }
 }
