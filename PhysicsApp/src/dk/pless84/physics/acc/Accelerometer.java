88a711f0781c269c0c12f503dbf3b1d5e49c8770
Jakob Pless <pless84@gmail.com>
2012-05-10 10:56:28 +0200

    FFT

---
+++ PhysicsApp/src/dk/pless84/physics/acc/Accelerometer.java
@@ -0,0 +1,20 @@
+package dk.pless84.physics.acc;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+
+public class Accelerometer implements SensorEventListener {
+
+	public Accelerometer() {
+		
+	}
+	
+	public void onAccuracyChanged(Sensor sensor, int val) {
+
+	}
+
+	public void onSensorChanged(SensorEvent sensor) {
+
+	}
+}
